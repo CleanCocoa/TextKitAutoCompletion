8a184593ee0c057628d885a3f5e33a1cfac3d10a
Christian Tietze <me@christiantietze.de>
2025-01-02 11:09:55 +0100

    add demo app project

---
+++ Demo/TextKitAutoCompletionDemo/AppDelegate.swift
@@ -0,0 +1,8 @@
+//  Copyright © 2025 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet var window: NSWindow!
+}
---
+++ Demo/TextKitAutoCompletionDemo/TextViewController.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2025 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import AppKit
+import TextKitAutoCompletion
+
+class TextViewController: NSViewController {
+    @IBOutlet weak var textView: NSTextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
