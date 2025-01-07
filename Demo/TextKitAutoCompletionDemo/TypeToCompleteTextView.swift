409e06bc86cee1a73b2e50a358ab63959f6f0b6c
Christian Tietze <me@christiantietze.de>
2025-01-07 13:05:40 +0100

    move popover positioning on insertion point to CompletionController

--- Demo/TextKitAutoCompletionDemo/TypeToCompleteTextView.swift
+++ Demo/TextKitAutoCompletionDemo/TypeToCompleteTextView.swift
@@ -26,8 +26,6 @@ class TypeToCompleteTextView: NSTextView {
     var isCompleting: Bool { completionController?.isCompleting ?? false }
 
     override func complete(_ sender: Any?) {
-        guard let window = self.window else { preconditionFailure("Views are expected to have windows") }
-
         let partialWordRange = self.rangeForUserCompletion
         /// Unused by our approach; selection is reflected in the completion window directly.
         var indexOfSelectedItem: Int = -1
@@ -41,15 +39,11 @@ class TypeToCompleteTextView: NSTextView {
         let completionController = CompletionController()
         defer { self.completionController = completionController }
 
-        let selectionRectInScreenCoordinates = self.firstRect(forCharacterRange: self.selectedRange(), actualRange: nil)
-        let selectionRectInWindow = window.convertFromScreen(selectionRectInScreenCoordinates)
-        let selectionRect = self.convert(selectionRectInWindow, from: nil)
         completionController.display(
-            completions: completions,
+            completionCandidates: completions,
             forPartialWordRange: partialWordRange,
             originalString: self.attributedString().attributedSubstring(from: partialWordRange).string,
-            relativeTo: selectionRect,
-            forTextView: self
+            relativeToInsertionPointOfTextView: self
         )
     }
 
@@ -80,7 +74,33 @@ class CompletionController: NSObject, NSPopoverDelegate {
     private(set) var isCompleting = false
 
     func display(
-        completions: [CompletionCandidate],
+        completionCandidates: [CompletionCandidate],
+        forPartialWordRange partialWordRange: NSRange,
+        originalString: String,
+        relativeToInsertionPointOfTextView textView: NSTextView
+    ) {
+        guard let window = textView.window else { preconditionFailure("Views on screen are expected to have windows") }
+
+        let selectionRectInScreenCoordinates = textView.firstRect(forCharacterRange: textView.selectedRange(), actualRange: nil)
+        let selectionRectInWindow = window.convertFromScreen(selectionRectInScreenCoordinates)
+        let selectionRectInTextView = textView.convert(selectionRectInWindow, from: nil)
+
+        display(
+            completionCandidates: completionCandidates,
+            forPartialWordRange: partialWordRange,
+            originalString: originalString,
+            relativeTo: selectionRectInTextView,
+            forTextView: textView
+        )
+    }
+
+    /// Display completion candidates in a text view.
+    ///
+    /// This mimicks, but doesn't match, the private `NSTextViewCompletionController` call, which would be:
+    ///
+    ///   -[NSTextViewCompletionController displayCompletions:indexOfSelectedItem:forPartialWordRange:originalString:atPoint:forTextView:]
+    func display(
+        completionCandidates: [CompletionCandidate],
         forPartialWordRange partialWordRange: NSRange,
         originalString: String,
         relativeTo rect: NSRect,
@@ -88,7 +108,9 @@ class CompletionController: NSObject, NSPopoverDelegate {
     ) {
         defer { isCompleting = true }
         var rect = rect
-        rect.size.width = max(rect.size.width, 1)  // Zero-width rect will be discarded and the popover will resort to showing on the view's edge.
+        // Insertion point is a zero-width rect, but these will be discarded and the popover will resort to showing on the view's edge. A height of _0_ I haven't encountered, yet, but for consistency make the same guarantee.
+        rect.size.width = max(rect.size.width, 1)
+        rect.size.height = max(rect.size.height, 1)
         popover.show(relativeTo: rect, of: textView, preferredEdge: .minY)
         controller.showCompletionCandidates(completionCandidates, in: textView)
     }
