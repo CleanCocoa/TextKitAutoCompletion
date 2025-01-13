db11bc92ccaeb122a86d03e03bddb756befaaa3d
Christian Tietze <me@christiantietze.de>
2025-01-13 07:20:21 +0100

    change demo setup to show how changing window first responder fails to dismiss popover

--- Demo/TextKitAutoCompletionDemo/AppDelegate.swift
+++ Demo/TextKitAutoCompletionDemo/AppDelegate.swift
@@ -5,4 +5,11 @@ import AppKit
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet var window: NSWindow!
+    @IBOutlet var textField: NSTextField!
+    @IBOutlet var textView: NSTextView!
+
+    @IBAction func focusTextField(_ sender: Any) {
+        // Used as the action of another responder in the window to interactively test whether auto-completion popover dismissal behaves correctly.
+        window.makeFirstResponder(textField)
+    }
 }
