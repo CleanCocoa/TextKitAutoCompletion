757ad16896c95c831604ccc9f892877bcebb4b41
Christian Tietze <me@christiantietze.de>
2025-01-24 11:18:03 +0100

    test some east-asian characters/words

--- Tests/TextKitAutoCompletionTests/WordRangeStrategyTests.swift
+++ Tests/TextKitAutoCompletionTests/WordRangeStrategyTests.swift
@@ -6,13 +6,12 @@ import TextBuffer
 import TextBufferTesting
 import AppKit
 
-@MainActor
+/// 'ZERO WIDTH JOINER'
+private let ZWJ = "\u{200D}"
+
 @Suite
 struct WordRangeStrategyTests: BufferTestsBase {
-    /// 'ZERO WIDTH JOINER'
-    let ZWJ = "\u{200D}"
-
-    let buffer: NSTextViewBuffer
+    @MainActor let buffer: NSTextViewBuffer
 
     init() {
         let textView = RangeConfigurableTextView(usingTextLayoutManager: false)
@@ -25,38 +24,71 @@ struct WordRangeStrategyTests: BufferTestsBase {
       arguments: [
         ("Helloˇ, World!", "«Hello», World!"),
         ("Hello, Worldˇ!", "Hello, «World»!"),
-        ("davie504ˇ",      "«davie504»"),
+        ("你好ˇ",           "«你好»"),
+        ("你   好ˇ",        "你   «好»"),
       ]
     )
     func expandsToWordBeforePoint(input: String, expected: String) throws {
         try expect(rangeOf: input, toBe: expected)
     }
 
-    @Test("expands to word part before point, ignoring remainder")
-    func expandsToWordPartBeforePoint() throws {
-        try expect(rangeOf: "Heˇllo, World!", toBe: "«He»llo, World!")
-        try expect(rangeOf: "Hello, Worˇld!", toBe: "Hello, «Wor»ld!")
+    @Test(
+      "expands to both letters and numbers before point",
+      arguments: [
+        ("davie504ˇ",      "«davie504»"),
+      ]
+    )
+    func expandsToLettersAndNumbersBeforePoint(input: String, expected: String) throws {
+        try expect(rangeOf: input, toBe: expected)
     }
 
-    @Test("does not skip over puctuation marks")
-    func ignorePunctuationMarks() throws {
-        try expect(rangeOf: "Hello,ˇ World!",  toBe: "Hello,ˇ World!")
-        try expect(rangeOf: "Hello, World!ˇ",  toBe: "Hello, World!ˇ")
-        try expect(rangeOf: "(Hello)ˇ World!", toBe:"(Hello)ˇ World!")
+    @Test(
+      "expands to word part before point, ignoring remainder",
+      arguments: [
+        ("Heˇllo, World!", "«He»llo, World!"),
+        ("Hello, Worˇld!", "Hello, «Wor»ld!"),
+      ])
+    func expandsToWordPartBeforePoint(input: String, expected: String) throws {
+        try expect(rangeOf: input, toBe: expected)
     }
 
-    @Test("expands to composite word with hyphen")
-    func expandsToWordWithHyphen() throws {
-        try expect(rangeOf: "The common-wealthˇ is poor", toBe: "The «common-wealth» is poor")
-        try expect(rangeOf: "The common-weaˇlth is poor", toBe: "The «common-wea»lth is poor")
-        try expect(rangeOf: "The commonˇ-wealth is poor", toBe: "The «common»-wealth is poor")
-        try expect(rangeOf: "The commˇon-wealth is poor", toBe: "The «comm»on-wealth is poor")
-        try expect(rangeOf: "Un\(ZWJ)commonˇ",            toBe: "«Un\(ZWJ)common»")
+    @Test(
+      "does not skip over punctuation marks",
+      arguments: [
+        ("Hello,ˇ World!",  "Hello,ˇ World!"),
+        ("Hello, World!ˇ",  "Hello, World!ˇ"),
+        ("你、ˇ好",          "你、ˇ好"),  // IDEOGRAPHIC COMMA
+        ("你。ˇ好",          "你。ˇ好"),  // IDEOGRAPHIC PERIOD
+        ("你､ˇ好",          "你､ˇ好"),  // HALFWIDTH IDEOGRAPHIC COMMA
+        ("你｡ˇ好",          "你｡ˇ好"),  // HALFWIDTH IDEOGRAPHIC PERIOD
+        ("(Hello)ˇ World!", "(Hello)ˇ World!"),
+        ("你   〔好〕ˇ",        "你   〔好〕ˇ"),
+        ("你   《好》ˇ",        "你   《好》ˇ"),
+      ])
+    func ignorePunctuationMarks(input: String, expected: String) throws {
+        try expect(rangeOf: input, toBe: expected)
     }
 
-    @Test("does not expand to only composition characters")
-    func ignoreCompositionOnly() throws {
-        try expect(rangeOf: "but --ˇ also", toBe: "but --ˇ also")
-        try expect(rangeOf: "but __ˇ also", toBe: "but __ˇ also")
+    @Test(
+      "expands to composite word with hyphen",
+      arguments: [
+        ("The common-wealthˇ is poor", "The «common-wealth» is poor"),
+        ("The common-weaˇlth is poor", "The «common-wea»lth is poor"),
+        ("The commonˇ-wealth is poor", "The «common»-wealth is poor"),
+        ("The commˇon-wealth is poor", "The «comm»on-wealth is poor"),
+        ("Un\(ZWJ)commonˇ",            "«Un\(ZWJ)common»"),
+      ])
+    func expandsToWordWithHyphen(input: String, expected: String) throws {
+        try expect(rangeOf: input, toBe: expected)
+    }
+
+    @Test(
+      "does not expand to only composition characters",
+      arguments: [
+        ("but --ˇ also", "but --ˇ also"),
+        ("but __ˇ also", "but __ˇ also"),
+      ])
+    func ignoreCompositionOnly(input: String, expected: String) throws {
+        try expect(rangeOf: input, toBe: expected)
     }
 }
